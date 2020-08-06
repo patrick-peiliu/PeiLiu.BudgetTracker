ce47f16e03e0935b6127b8692d0308428e44ad6e
patrick-peiliu <patrickliupack@gmail.com>
2020-08-06 19:45:42 -0400

    Added Database Scripts

---
+++ DbScripts/Users.sql
@@ -0,0 +1,23 @@
+USE [PeiLiu.BudgetTracker]
+GO
+SET IDENTITY_INSERT [dbo].[Users] ON 
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (12, N'Verla.Yost@gmail.com', N'QWErty@123', N'Anthony')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (13, N'Tencent.Yost@gmail.com', N'QWErty@123', N'Bob')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (14, N'Cindy.Yost@gmail.com', N'QWErty@123', N'Cindy')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (15, N'Camby.Yost@gmail.com', N'QWErty@123', N'Camby')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (16, N'Mac.Yost@gmail.com', N'QWErty@123', N'Mac')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (17, N'Donald.Yost@gmail.com', N'QWErty@123', N'Donald')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (18, N'Jack.Yost@gmail.com', N'QWErty@123', N'Jack')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (19, N'Jacob.Yost@gmail.com', N'QWErty@123', N'Jacob')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (20, N'Nancy.Yost@gmail.com', N'QWErty@123', N'Nancy')
+INSERT [dbo].[Users] ([Id], [Email], [Password], [FullName]) 
+	VALUES (21, N'Vicky.Yost@gmail.com', N'QWErty@123', N'Vicky')
